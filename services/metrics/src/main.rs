7b963c96702b4cd08fbe6f536f1d3bf636f904d8
Til Blechschmidt <til@blechschmidt.de>
2020-07-02 22:19:04 +0200

    :recycle: Move metrics to new scheduling system
    Currently this is only a placeholder and will be implemented later.

--- services/metrics/src/main.rs
+++ services/metrics/src/main.rs
@@ -1,60 +1,85 @@
-use redis::aio::ConnectionManager;
-use warp::Filter;
+// use redis::aio::ConnectionManager;
+// use warp::Filter;
 
+// use log::info;
+// use shared::database::connect;
+// use shared::ports::ServicePort;
+// use shared::service_init;
+
+// mod config;
+// mod data_collector;
+// mod structures;
+
+// use crate::config::Config;
+// use data_collector::*;
+// use structures::*;
+
+// async fn handle_post(con: ConnectionManager) -> Result<impl warp::Reply, warp::Rejection> {
+//     let metrics: Vec<String> = vec![
+//         proxy_requests(&con).await,
+//         proxy_traffic(&con).await,
+//         session_log(&con).await,
+//         session_startup_duration(&con).await,
+//         // TODO Replace later with session_total{stage="queued|pending|alive|terminated"} counter
+//         sessions_active(&con).await,
+//     ]
+//     .iter()
+//     .map(|metric| format!("{}", metric))
+//     .collect();
+
+//     Ok(warp::reply::with_status(
+//         metrics.join("\n"),
+//         warp::http::StatusCode::OK,
+//     ))
+// }
+
+// #[tokio::main]
+// async fn main() {
+//     service_init();
+
+//     let config = Config::new().unwrap();
+
+//     let con = connect(config.clone().redis_url).await;
+
+//     let heart = shared::lifecycle::Heart::new(&con, None);
+
+//     let con_clone = con.clone();
+//     let with_con = warp::any().map(move || con_clone.clone());
+//     let session_route = warp::get()
+//         .and(warp::path("metrics"))
+//         .and(with_con)
+//         .and_then(handle_post);
+
+//     let listening_socket = ServicePort::Metrics.socket_addr();
+//     info!("Listening at {:?}", listening_socket);
+//     let server = warp::serve(session_route).run(listening_socket);
+
+//     tokio::spawn(server);
+
+//     heart.beat(true).await;
+// }
+
+use lifecycle::{service_init, Heart};
 use log::info;
-use shared::database::connect;
-use shared::ports::ServicePort;
-use shared::service_init;
+use scheduling::{schedule, JobScheduler, StatusServer};
 
-mod config;
-mod data_collector;
-mod structures;
-
-use crate::config::Config;
-use data_collector::*;
-use structures::*;
-
-async fn handle_post(con: ConnectionManager) -> Result<impl warp::Reply, warp::Rejection> {
-    let metrics: Vec<String> = vec![
-        proxy_requests(&con).await,
-        proxy_traffic(&con).await,
-        session_log(&con).await,
-        session_startup_duration(&con).await,
-        // TODO Replace later with session_total{stage="queued|pending|alive|terminated"} counter
-        sessions_active(&con).await,
-    ]
-    .iter()
-    .map(|metric| format!("{}", metric))
-    .collect();
-
-    Ok(warp::reply::with_status(
-        metrics.join("\n"),
-        warp::http::StatusCode::OK,
-    ))
-}
+#[derive(Clone)]
+struct DummyContext {}
 
 #[tokio::main]
 async fn main() {
     service_init();
 
-    let config = Config::new().unwrap();
+    let scheduler = JobScheduler::new();
+    let (mut heart, _) = Heart::new();
+    let context = DummyContext {};
 
-    let con = connect(config.clone().redis_url).await;
+    let status_job = StatusServer::new(&scheduler);
 
-    let heart = shared::lifecycle::Heart::new(&con, None);
+    schedule!(scheduler, context, { status_job });
 
-    let con_clone = con.clone();
-    let with_con = warp::any().map(move || con_clone.clone());
-    let session_route = warp::get()
-        .and(warp::path("metrics"))
-        .and(with_con)
-        .and_then(handle_post);
+    let death_reason = heart.death().await;
+    info!("Heart died: {}", death_reason);
 
-    let listening_socket = ServicePort::Metrics.socket_addr();
-    info!("Listening at {:?}", listening_socket);
-    let server = warp::serve(session_route).run(listening_socket);
-
-    tokio::spawn(server);
-
-    heart.beat(true).await;
+    scheduler.terminate_jobs().await;
 }
