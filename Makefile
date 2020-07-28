ef37daf6522cd1e36e1d31b7cadab12d30a90ff9
Til Blechschmidt <til@blechschmidt.de>
2020-07-28 14:26:29 +0200

    :hammer: Collate build artifacts into repository root

--- Makefile
+++ Makefile
@@ -1,20 +1,27 @@
-.PHONY: core api clean
+.PHONY: build-core build-api clean
 
-core:
+build: build-api build-core
+bundle: bundle-api bundle-core
+
+build-api:
+	cd api && yarn install && yarn build
+	mkdir -p .artifacts/api-executable
+	mv api/dist/index.js .artifacts/api-executable
+
+build-core:
 	cd core && ./build.sh
 
-webgrid: core
-	docker build -f distribution/docker/images/core/Dockerfile -t webgrid .
+bundle-api: build-api
+	docker build -f distribution/docker/images/api/Dockerfile -t webgrid-api .
 
-node: core
+bundle-core: build-core
+	docker build -f distribution/docker/images/core/Dockerfile -t webgrid-core .
+
+bundle-node: build-core
 	docker build -f distribution/docker/images/node/Dockerfile -t webgrid-node-firefox --build-arg browser=firefox .
 	docker build -f distribution/docker/images/node/Dockerfile -t webgrid-node-chrome --build-arg browser=chrome .
 
-api:
-	docker build -f distribution/docker/images/api/Dockerfile -t webgrid-api .
-
-images: node webgrid api
-
 clean:
-	rm -rf core/.build core/.cache core/target
+	rm -rf .artifacts
+	rm -rf core/.cache core/target
 	rm -rf api/dist api/node_modules api/src/generated.ts
--- core/build.sh
+++ core/build.sh
@@ -1,25 +1,39 @@
 #!/usr/bin/env bash
 SOURCE_DIR=$(pwd)
 BUILD_DIR=$(pwd)/.cache
+TARGET_DIR=$(pwd)/../.artifacts
 
-echo "Building @ $BUILD_DIR"
+set -e
+
+echo "Creating cache directories in $BUILD_DIR"
 mkdir -p $BUILD_DIR
 mkdir -p $BUILD_DIR/project
 mkdir -p $BUILD_DIR/cargo-git
 mkdir -p $BUILD_DIR/cargo-registry
 mkdir -p $BUILD_DIR/target
 
-rsync -av --progress $SOURCE_DIR/* $BUILD_DIR/project --exclude target --exclude .build --exclude .cache
+# chmod -R 777 $BUILD_DIR 
 
-docker run --rm -it \
+echo "Copying project to build cache"
+rsync -a --progress $SOURCE_DIR/* $BUILD_DIR/project --exclude target --exclude .build --exclude .cache
+
+echo "Running build in webgrid/rust-musl-builder container"
+docker run --rm \
 	-v "$BUILD_DIR/project":/home/rust/src \
 	-v "$BUILD_DIR/cargo-git":/home/rust/.cargo/git \
 	-v "$BUILD_DIR/cargo-registry":/home/rust/.cargo/registry \
 	-v "$BUILD_DIR/target":/home/rust/src/target \
-	ekidd/rust-musl-builder \
-	cargo build --release --workspace --locked
+	-e CARGO_TERM_COLOR=always \
+	webgrid/rust-musl-builder \
+	bash -c "cargo build --release --workspace --locked && cargo doc --release --workspace --locked --no-deps --document-private-items && rm /home/rust/src/target/x86_64-unknown-linux-musl/doc/.lock"
 
 # TODO: Strip debug symbols from binary
 
-mkdir -p $SOURCE_DIR/.build
-find $BUILD_DIR/target/x86_64-unknown-linux-musl/release -type f -perm 0755 -maxdepth 1 -exec cp {} $SOURCE_DIR/.build/ \;
+echo "Creating output directories in $TARGET_DIR"
+mkdir -p $TARGET_DIR/core-documentation
+mkdir -p $TARGET_DIR/core-executable
+
+echo "Copying documentation to output"
+rsync -a --progress $BUILD_DIR/target/x86_64-unknown-linux-musl/doc $TARGET_DIR/core-documentation
+echo "Copying executable to output"
+rsync -av --progress $BUILD_DIR/target/x86_64-unknown-linux-musl/release/webgrid $TARGET_DIR/core-executable
