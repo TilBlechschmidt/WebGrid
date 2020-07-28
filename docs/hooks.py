e4deefeaf544e1ead0b7ff55ac694ebf83723724
Til Blechschmidt <til@blechschmidt.de>
2020-07-28 14:28:02 +0200

    :pencil: Include core rust documentation in mkdocs page

--- docs/hooks.py
+++ docs/hooks.py
@@ -1,6 +1,10 @@
 import shutil
 import os
 
-def copy_readme(*args, **kwargs):
+def on_post_build(*args, **kwargs):
+    # Copy latest docker compose file
     site_dir = kwargs['config']['site_dir']
     shutil.copy("distribution/docker/docker-compose.yml", os.path.join(site_dir, "docker-compose.yml"))
+
+    # Copy core documentation to /components/core
+    shutil.copytree(".artifacts/core-documentation/doc", os.path.join(site_dir, "rust-doc"))
